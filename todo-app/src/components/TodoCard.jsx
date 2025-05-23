b71975316858e9645fe939b1ab66bb57a159006f
Danny Zhang <dtz24@drexel.edu>
2025-05-23 11:29:49 -0400

    completed a while ago, forgot to push. Added functionaility to app.

--- todo-app/src/components/TodoCard.jsx
+++ todo-app/src/components/TodoCard.jsx
@@ -1,5 +1,15 @@
-export function TodoCard() {
+export function TodoCard(props) {
+    const { todo, handleDeleteTodo, handleCompleteTodo, todoIndex } = props
+    
     return (
-        <div></div>
+        <div className = "card todo-item"> 
+            <p>{todo.input}</p>
+            <div className="todo-buttons">
+                <button onClick={()=>{handleCompleteTodo(todoIndex)}} disabled={todo.complete}> <h6>Done</h6></button>
+                <button onClick={()=> {
+                    handleDeleteTodo(todoIndex)
+                }}> <h6>Delete</h6></button>
+            </div>
+        </div>
     )
 }
